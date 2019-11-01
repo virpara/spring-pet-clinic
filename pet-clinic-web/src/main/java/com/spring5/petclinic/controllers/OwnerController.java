987a429f61427f7ed77478430c4e23b500468215
virpara <virpararahul@gmail.com>
2019-11-01 21:21:29 +0530

    Implement controller and view to list pet owners. Closes #16

--- pet-clinic-web/src/main/java/com/spring5/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/spring5/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.spring5.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring5.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping({"/owners"})
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/index", "/index.html"})
-	public String owners() {
+	public String owners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		
 		return "owners/index";
 	}
